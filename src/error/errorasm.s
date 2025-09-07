3df37b5bc66a50a5f4e29ed6916f0cce589c3777
CrashOveride95 <crashoveride953@gmail.com>
2025-09-06 20:47:22 -0400

    Remove outdated code from errorasm/exceptasm

--- src/error/errorasm.s
+++ src/error/errorasm.s
@@ -6,26 +6,9 @@
 #include "PR/os_version.h"
 
 LEAF(__osError)
-#if BUILD_VERSION < VERSION_J
-    lw      t0, __osErrorHandler
-    beqz    t0, 1f
-    jr      t0
-#elif BUILD_VERSION < VERSION_K
-    lw      t0, __kmc_pt_mode
-    bnez    t0, _kmc_mode
-
-    lw      t0, __osErrorHandler
-    beqz    t0, 1f
-    jr      t0
-_kmc_mode:
-    lw      t0, __kmcErrorHandler
-    beqz    t0, 1f
-    jr      t0
-#else
     lw      t0, __osCommonHandler
     beqz    t0, 1f
     jr      t0
-#endif
 1:
     j       ra
 END(__osError)
